188b226ef1189b911219edd2f77d80b4990f1406
SnowyOwl25 <97636861+SnowyOwl25@users.noreply.github.com>
2022-01-14 23:44:25 +0200

    Add files via upload

--- js/loader.js
+++ js/loader.js
@@ -6,8 +6,11 @@ $(document).ready(function(){
     fullWidth: true,
     $('.carousel').carousel('next');}, 10000);
   
-  $(document).ready(function(){
-    $('.fixed-action-btn').floatingActionButton();
+  document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.fixed-action-btn');
+    var instances = M.FloatingActionButton.init(elems, {
+      hoverEnabled: false
+    });
   });
 
   $(document).ready(function(){
