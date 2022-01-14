ea78e0a8a0e8306bd530e582c005b5d3c1135551
SnowyOwl25 <97636861+SnowyOwl25@users.noreply.github.com>
2022-01-14 23:10:59 +0200

    Add files via upload

--- js/loader.js
+++ js/loader.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
     $('.sidenav').sidenav();
   });
+  
   $('.carousel').carousel(); setInterval(function() {
     fullWidth: true,
     $('.carousel').carousel('next');}, 10000);
+  
+  $(document).ready(function(){
+    $('.fixed-action-btn').floatingActionButton();
+  });
+
+  $(document).ready(function(){
+    $('.modal').modal();
+  });
